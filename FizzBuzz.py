4aaa681718317992e67cbb55a678c4257e8af9df
MALLAREDDY VINAYAK RAO <46836083+vr-vinayak@users.noreply.github.com>
2022-11-04 15:19:26 +0530

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -2,6 +2,6 @@ for counter in range(1,60):
     if counter%3==0:
         print('Fizz Multiple of only 3')
     if counter%5==0:
-        print('Buzz Multiple of only 5!!!')
+        print('Buzz Mfbrhbfbfhbfsebfsufbhewrjfultiple of only 5!!!')
     if counter%3==0 and counter%5==0:
         print('FizzBuzz Multiple of 3 &  5!!!')
