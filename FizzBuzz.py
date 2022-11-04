2f8242d28bcaedf29f145bf55f0c7d9a4a8ca29a
MALLAREDDY VINAYAK RAO <46836083+vr-vinayak@users.noreply.github.com>
2022-11-04 13:38:03 +0530

    first commit

---
+++ FizzBuzz.py
@@ -0,0 +1,7 @@
+for counter in range(1,100):
+    if counter%3==0:
+        print('Fizz Multiple of only 3')
+    if counter%5==0:
+        print('Buzz Multiple of only 5')
+    if counter%3==0 and counter%5==0:
+        print('FizzBuzz Multiple of 3 &  5')
