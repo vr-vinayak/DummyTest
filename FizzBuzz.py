9ff5c313551d3d99c63a2e4da4c005b8b5262efe
MALLAREDDY VINAYAK RAO <46836083+vr-vinayak@users.noreply.github.com>
2022-11-04 16:42:25 +0530

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,7 +1,5 @@
-for counter in range(1,60):
-    if counter%3==0:
-        print('Fizz Multiple of only 3')
-    if counter%5==0:
-        print('Buzz Mfbrhbfbfhbfsebfsufbhewrjfultiple of only 5!!!')
-    if counter%3==0 and counter%5==0:
-        print('FizzBuzz Multiple of 3 &  5!!!')
+def inc(x):
+    return x + 1
+
+def test_answer():
+    assert inc(3) == 5
