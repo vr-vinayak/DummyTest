9012d983034bde632e7e2ae344090a75d4cea257
vr-vinayak <raovinayak07@gmail.com>
2022-11-07 12:44:46 +0530

    Changes done!!

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,5 +1,13 @@
-def inc(x):
-    return x+1
+'''
+Simple test case
+'''
+
+def inc(base_x):
+    '''simple increment function'''
+    print('2nd test!!!')
+    return base_x + 1
 
 def test_answer():
+    '''simple base function'''
+    print('2nd test')
     assert inc(4) == 5
