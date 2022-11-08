95258d0964cc5351c82e83a27f28c5ea884ab769
vr-vinayak <raovinayak07@gmail.com>
2022-11-08 12:11:27 +0530

    File name renamed

---
+++ fizz_buzz.py
@@ -0,0 +1,11 @@
+'''Fizz Buzz Program'''
+
+def inc(base_x):
+    '''Increment Function'''
+    print('2nd test!!!')
+    return base_x + 1
+
+def test_answer():
+    '''simple base function'''
+    print('2nd test')
+    assert inc(4) == 5
